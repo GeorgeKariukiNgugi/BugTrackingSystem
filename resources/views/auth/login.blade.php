6c22c64c3bf5121785d89e92e120d18bb3794cce
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-03-03 22:26:10 +0300

    Added a new logIn Page and also a new navigation bar with all notifications and the messaging present

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,134 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BugTracker</title>
+    <link rel="stylesheet" href="studioTemplates/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="studioTemplates/assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="studioTemplates/assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="studioTemplates/assets/css/Bold-BS4-Jumbotron-with-Particles-js.css">
+    <link rel="stylesheet" href="studioTemplates/assets/css/Bold-BS4-Jumbotron-with-Particles-js1.css">
+    <link rel="stylesheet" href="studioTemplates/assets/css/Footer-Dark.css">
+    <link rel="stylesheet" href="studioTemplates/assets/css/Login-Form-Dark.css">
+    <link rel="stylesheet" href="studioTemplates/assets/css/Navigation-Clean.css">
+    <link rel="stylesheet" href="studioTemplates/assets/css/Navigation-with-Search.css">
+    <link rel="stylesheet" href="studioTemplates/assets/css/Projects-Horizontal.css">
+    <link rel="stylesheet" href="studioTemplates/assets/css/Social-Icons.css">
+    <link rel="stylesheet" href="studioTemplates/assets/css/styles.css">
+    <link href="{{ asset('css/customCSS.css') }}" rel="stylesheet">
+</head>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+<body>
+    <div class="login-dark">
+        <div style="margin-bottom:1%;margin-top:0%;background-color:#4D04C5;">
+            <nav class="navbar navbar-light navbar-expand-md navigation-clean" style="background-color:#6367E3;color:rgb(255,255,255);">
+                <div class="container"><a class="navbar-brand" href="/">{{ config('app.name', 'Laravel') }}</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse"
+                        id="navcol-1">
+                        <ul class="nav navbar-nav ml-auto">
+                            <li class="nav-item" role="presentation"><a class="nav-link" href="#" style="color:rgb(255,255,255);"><strong>Home</strong></a></li>
+                            <li class="nav-item" role="presentation"><a class="nav-link" href="#" style="color:rgb(255,255,255);"><strong>Contacts</strong></a></li>
+                            <li class="nav-item" role="presentation">
+                            @guest
+                                    <li class="nav-item">
+                                        <a class="nav-link" style="color:white;text-style:bold;" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    </li>
+                                    @if (Route::has('register'))
+                                        <li class="nav-item">
+                                            <a class="nav-link" style="color:white;text-style:bold;" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                        </li>
+                                    @endif
+                                @else
+                                    <li class="nav-item dropdown">
+                                        <a id="navbarDropdown" style="color:white;text-style:bold;" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                            {{ Auth::user()->name }} <span class="caret"></span>
+                                        </a>
+    
+                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                                onclick="event.preventDefault();
+                                                                document.getElementById('logout-form').submit();">
+                                                {{ __('Logout') }}
+                                            </a>
+    
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                @csrf
+                                            </form>
+                                        </div>
+                                    </li>
+                              @endguest
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
+            </nav>
         </div>
+        
+        <form method="POST" action="{{ route('login') }}" style="background-color:#4a51f2;">
+            @csrf
+            <h2 class="sr-only">Login Form</h2>
+            <div class="illustration"><i class="icon ion-ios-locked-outline" style="color:rgb(255,255,255);">
+            </i></div>
+            <div class="form-group">
+                <input placeholder="Email" id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+            
+            @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+
+            </div>
+            <div class="form-group">
+                <input id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="form-group"><button class="btn btn-dark btn-block" type="submit">Log In</button></div>
+            <a href="#" class="forgot" style="color:rgb(255,255,255);">Forgot your email or password?</a>
+        </form>
     </div>
-</div>
-@endsection
+
+    <div class="footer-dark" style="background-color:rgb(77,4,197);">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Services</h3>
+                        <ul>
+                            <li><a href="#">Web design</a></li>
+                            <li><a href="#">Development</a></li>
+                            <li><a href="#">Hosting</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>About</h3>
+                        <ul>
+                            <li><a href="#">Company</a></li>
+                            <li><a href="#">Team</a></li>
+                            <li><a href="#">Careers</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>Company Name</h3>
+                        <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+                    </div>
+                    <div class="col item social"><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-instagram"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
+                </div>
+                <p class="copyright">Company Name © 2017</p>
+            </div>
+        </footer>
+    </div>
+
+    <script src="studioTemplates/assets/js/jquery.min.js"></script>
+    <script src="studioTemplates/assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="studioTemplates/assets/js/Bold-BS4-Jumbotron-with-Particles-js.js"></script>
+</body>
+
+</html>
--- resources/views/extending/landingPage.blade.php
+++ resources/views/extending/landingPage.blade.php
@@ -15,16 +15,21 @@
     <link rel="stylesheet" href="studioTemplates/assets/css/Projects-Horizontal.css">
     <link rel="stylesheet" href="studioTemplates/assets/css/Social-Icons.css">
     <link rel="stylesheet" href="studioTemplates/assets/css/styles.css">
+    {{-- <link rel="stylesheet" href="{{asset('AdminLTE-master/plugins/fontawesome-free/css/all.min.css')}}"> --}}
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{asset('AdminLTE-master/dist/css/adminlte.min.css')}}">
+
+  <link href="{{ asset('css/customCSS.css') }}" rel="stylesheet">
 </head>
 
 <body>
-    <div style="margin-bottom:1%;margin-top:0%;background-color:#4d04c5;">
+    <div style="margin-bottom:1%;margin-top:0%;background-color:#4D04C5;">
         <nav class="navbar navbar-light navbar-expand-md navigation-clean" style="background-color:#6367E3;color:rgb(255,255,255);">
-            <div class="container"><a class="navbar-brand" href="#">{{ config('app.name', 'Laravel') }}</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="container"><a class="navbar-brand" href="/">{{ config('app.name', 'Laravel') }}</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse"
                     id="navcol-1">
                     <ul class="nav navbar-nav ml-auto">
-                        <li class="nav-item" role="presentation"><a class="nav-link active" href="#" style="color:rgb(255,255,255);"><strong>Home</strong></a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="#" style="color:rgb(255,255,255);"><strong>Home</strong></a></li>
                         <li class="nav-item" role="presentation"><a class="nav-link" href="#" style="color:rgb(255,255,255);"><strong>Contacts</strong></a></li>
                         <li class="nav-item" role="presentation">
                         @guest
@@ -37,6 +42,97 @@
                                     </li>
                                 @endif
                             @else
+
+
+                            {{-- This section of the code is used to get the notification section of the navigation 
+                                bar that includes the messages and laso the normal notifications. --}}
+
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link" data-toggle="dropdown" href="#" style="color:white;">
+                                        <i class="fa fa-comments"></i>
+                                      <span class="badge badge-danger navbar-badge">3</span>
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                                      <a href="#" class="dropdown-item">
+                                        <!-- Message Start -->
+                                        <div class="media">
+                                          <img src="AdminLTE-master/dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+                                          <div class="media-body">
+                                            <h3 class="dropdown-item-title">
+                                              Brad Diesel
+                                              <span class="float-right text-sm text-danger"><i class="fa fa-star"></i></span>
+                                            </h3>
+                                            <p class="text-sm">Call me whenever you can...</p>
+                                            <p class="text-sm text-muted"><i class="fa fa-clock mr-1"></i> 4 Hours Ago</p>
+                                          </div>
+                                        </div>
+                                        <!-- Message End -->
+                                      </a>
+                                      <div class="dropdown-divider"></div>
+                                      <a href="#" class="dropdown-item">
+                                        <!-- Message Start -->
+                                        <div class="media">
+                                          <img src="AdminLTE-master/dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                                          <div class="media-body">
+                                            <h3 class="dropdown-item-title">
+                                              John Pierce
+                                              <span class="float-right text-sm text-muted"><i class="fa fa-star"></i></span>
+                                            </h3>
+                                            <p class="text-sm">I got your message bro</p>
+                                            <p class="text-sm text-muted"><i class="fa fa-clock mr-1"></i> 4 Hours Ago</p>
+                                          </div>
+                                        </div>
+                                        <!-- Message End -->
+                                      </a>
+                                      <div class="dropdown-divider"></div>
+                                      <a href="#" class="dropdown-item">
+                                        <!-- Message Start -->
+                                        <div class="media">
+                                          <img src="AdminLTE-master/dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                                          <div class="media-body">
+                                            <h3 class="dropdown-item-title">
+                                              Nora Silvester
+                                              <span class="float-right text-sm text-warning"><i class="fa fa-star"></i></span>
+                                            </h3>
+                                            <p class="text-sm">The subject goes here</p>
+                                            <p class="text-sm text-muted"><i class="fa fa-clock mr-1"></i> 4 Hours Ago</p>
+                                          </div>
+                                        </div>
+                                        <!-- Message End -->
+                                      </a>
+                                      <div class="dropdown-divider"></div>
+                                      <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+                                    </div>
+                                  </li>
+
+
+                                  <li class="nav-item dropdown">
+                                    <a class="nav-link" style="color:white;" data-toggle="dropdown" href="#">
+                                      <i class="fa fa-bell"></i>
+                                      <span class="badge badge-warning navbar-badge">15</span>
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                                      <span class="dropdown-header">15 Notifications</span>
+                                      <div class="dropdown-divider"></div>
+                                      <a href="#" class="dropdown-item">
+                                        <i class="fa fa-envelope mr-2"></i> 4 new messages
+                                        <span class="float-right text-muted text-sm">3 mins</span>
+                                      </a>
+                                      <div class="dropdown-divider"></div>
+                                      <a href="#" class="dropdown-item">
+                                        <i class="fa fa-users mr-2"></i> 8 friend requests
+                                        <span class="float-right text-muted text-sm">12 hours</span>
+                                      </a>
+                                      <div class="dropdown-divider"></div>
+                                      <a href="#" class="dropdown-item">
+                                        <i class="fa fa-file mr-2"></i> 3 new reports
+                                        <span class="float-right text-muted text-sm">2 days</span>
+                                      </a>
+                                      <div class="dropdown-divider"></div>
+                                      <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+                                    </div>
+                                  </li>
+
                                 <li class="nav-item dropdown">
                                     <a id="navbarDropdown" style="color:white;text-style:bold;" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                         {{ Auth::user()->name }} <span class="caret"></span>
@@ -124,6 +220,85 @@
         </div>
     </div>
     <div></div>
+
+    <div class="content "  style="background-color:rgb(197,197,197);">
+        <div class="container ">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="card card-outline card-danger">
+            <div class="card-header">
+              <h5 class="m-0 text-center">How To Report A bug.</h5>
+            </div>
+            <div class="card-body">
+              <h6 class="card-title">Steps to reporting a bug successfilly.</h6>
+              <ol type="i" class="card-text">
+                <li>Step 1</li>
+                <li>Step 2</li>
+                <li>Step 3</li>
+                <li>Step 4</li>
+            </ol>
+  
+              <a class="btn btn-danger custom-button-center" data-target = "#sampleModal" data-toggle="modal" href="#"> <i class = "fa fa-video-camera"></i> Watch Video.</a>
+            </div>
+          </div>
+  
+        </div><!-- /.col-md-6 -->
+        
+        <div class="col-lg-6">
+          <div class="card card-outline card-warning">
+            <div class="card-header">
+              <h5 class="m-0 text-center">How To Track Reported Bug End To End.</h5>
+            </div>
+            <div class="card-body">
+              <h6 class="card-title"> Steps to tracking a reported bug.</h6>
+              <ol type="i" class="card-text">
+                <li>Step 1</li>
+                <li>Step 2</li>
+                <li>Step 3</li>
+                <li>Step 4</li>
+            </ol>
+              <a class="btn btn-warning custom-button-center" data-target = "#sampleModal2" data-toggle="modal" href="#"> <i class = "fa fa-video-camera"> </i> Watch Video.</a>
+            </div>
+          </div>
+  
+        </div><!-- /.col-md-6 -->
+      </div>
+    </div><!-- /.container-fluid -->
+  </div>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+  </div>
+  <!-- ./wrapper -->
+        {{-- This section of the code is used to add the modals to the landing page.  --}} 
+
+    <div role="dialog" tabindex="-1" class="modal fade" id="sampleModal">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header" style="background-color:rgb(252,188,70)">
+                    <h4 class="modal-title" style="text-align:center;">How To Report A bug.</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                </div>
+                <div class="modal-body" style="background-color:rgb(244,196,108)"><video controls width="100%" height="100%" src="{{asset('videos/Record1.mp4')}}"></video></div>
+                <div class="modal-footer" style="background-color:rgb(252,188,70)"><button class="btn btn-danger active btn-sm" type="button" data-dismiss="modal">Close</button></div>
+            </div>
+        </div>
+    </div>
+    
+    
+    <div role="dialog" tabindex="-1" class="modal fade" id="sampleModal2">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header" style="background-color:rgb(249,64,64)">
+                    <h4 class="modal-title" style="text-align:center;">How To Report A bug.</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                </div>
+                <div class="modal-body" style="background-color:rgb(244,108,108)"><video controls width="100%" height="100%" src="{{asset('videos/Record1.mp4')}}"></video></div>
+                <div class="modal-footer" style="background-color:rgb(249,64,64)"><button class="btn btn-danger active btn-sm" type="button" data-dismiss="modal">Close</button></div>
+            </div>
+        </div>
+    </div>
+
     <div class="footer-dark" style="background-color:rgb(77,4,197);">
         <footer>
             <div class="container">
@@ -154,16 +329,7 @@
             </div>
         </footer>
     </div>
-    <div class="modal fade" role="dialog" tabindex="-1">
-        <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content">
-                <div class="modal-header" style="background-color:rgb(249,64,64);">
-                    <h4 class="text-center modal-title">First Video .</h4><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button></div>
-                <div class="modal-body"><video width="100%" height="100%" controls=""></video></div>
-                <div class="modal-footer"><button class="btn btn-danger active btn-sm" type="button" data-dismiss="modal">Close</button></div>
-            </div>
-        </div>
-    </div>
+
     <script src="studioTemplates/assets/js/jquery.min.js"></script>
     <script src="studioTemplates/assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="studioTemplates/assets/js/Bold-BS4-Jumbotron-with-Particles-js.js"></script>
